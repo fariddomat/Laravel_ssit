a2de12f2c919f38fb0b6a97d073051118fa17d97
Farid Domat <fariddomat.000@gmail.com>
2024-03-02 12:26:38 +0300

    init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Providers;
+
+use App\Color;
+use App\ContactInfo;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use App\SocialMedia;
+use App\Info;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        Schema::defaultStringLength(191);
+        // Using closure based composers...
+        View::composer('layouts.site', function ($view) {
+            $view->with('socialMedias', SocialMedia::all())
+                ->with('info', Info::find(1))
+                ->with('contactInfo', ContactInfo::find(1))
+                ->with('color', Color::find(1));
+        });
+
+
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.site')
+@section('title', trans('site.home'))
+@section('styles')
+
+@endsection
+@section('scripts')
+
+@endsection
+@section('content')
+
+
+@endsection
